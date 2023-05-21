7b8ed694abc8a46a9da8a2cf26027efc21c59a11
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-05-22 01:17:47 +0300

    register view created

--- ToDoList/Views/RegisterView.swift
+++ ToDoList/Views/RegisterView.swift
@@ -8,47 +8,46 @@
 import SwiftUI
 
 struct RegisterView: View {
-  @State var name = ""
-  @State var email = ""
-  @State var password = ""
+  @StateObject var viewModel = RegisterViewViewModel()
+
   
-    var body: some View {
-      VStack {
-        // Header
-        HeaderView(title: "Register",
-                   subTitle: "Start Organizing Todos" ,
-                   angle: -15,
-                   background: Color.green)
-        // Register
+  var body: some View {
+    VStack {
+      // Header
+      HeaderView(title: "Register",
+                 subTitle: "Start Organizing Todos" ,
+                 angle: -15,
+                 background: Color.green)
+      // Register
+      
+      Form {
+        TextField("Full Name", text: $viewModel.name)
+          .textFieldStyle(DefaultTextFieldStyle())
+          .autocorrectionDisabled()
         
-        Form {
-          TextField("Full Name", text: $name)
-            .textFieldStyle(DefaultTextFieldStyle())
-            .autocorrectionDisabled()
-          
-          TextField("Email Address", text: $email)
-            .textFieldStyle(DefaultTextFieldStyle())
-            .autocapitalization(.none)
-            .autocorrectionDisabled()
-          
-          SecureField("Password", text: $password)
-            .textFieldStyle(DefaultTextFieldStyle())
-          
-          TLButton(
-            title: "Register",
-            background: .green
-          ) {
-            // Attempt Registration
-          }.padding()
-        }
-        Spacer()
+        TextField("Email Address", text: $viewModel.email)
+          .textFieldStyle(DefaultTextFieldStyle())
+          .autocapitalization(.none)
+          .autocorrectionDisabled()
         
+        SecureField("Password", text: $viewModel.password)
+          .textFieldStyle(DefaultTextFieldStyle())
+        
+        TLButton(
+          title: "Register",
+          background: .green
+        ) {
+          viewModel.register()
+        }.padding()
       }
+      Spacer()
+      
     }
+  }
 }
 
 struct RegisterView_Previews: PreviewProvider {
-    static var previews: some View {
-        RegisterView()
-    }
+  static var previews: some View {
+    RegisterView()
+  }
 }
