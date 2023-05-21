9139794fb2b962efb256565c2d9c715f892bf89f
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-05-22 01:16:48 +0300

    extension added for change data to json

--- ToDoList/Other/Extensions.swift
+++ ToDoList/Other/Extensions.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+extension Encodable {
+  func asDictionary() -> [String: Any] {
+    guard let  data = try? JSONEncoder().encode(self) else {
+      return [:]
+    }
+    do {
+      let json = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+      return json ?? [:]
+    } catch {
+      return [:]
+    }
+  }
+}
