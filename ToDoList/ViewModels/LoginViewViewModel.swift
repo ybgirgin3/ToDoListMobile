e09af210bde915b97c4cc277324c9b33dd350497
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-05-22 00:26:36 +0300

    email validation added

--- ToDoList/ViewModels/LoginViewViewModel.swift
+++ ToDoList/ViewModels/LoginViewViewModel.swift
@@ -5,4 +5,37 @@
 //  Created by Yusuf Berkay Girgin on 21.05.2023.
 //
 
+import FirebaseAuth
 import Foundation
+
+class LoginViewViewModel: ObservableObject {
+  @Published var email = ""
+  @Published var password = ""
+  @Published var errorMessage = ""
+  
+  init() {}
+  
+  func login() {
+    guard validate() else {
+      return
+    }
+    
+    // Try to login
+    Auth.auth().signIn(withEmail: email, password: password)
+    
+  }
+  
+  private func validate() -> Bool {
+    errorMessage = ""
+    guard !email.trimmingCharacters(in: .whitespaces).isEmpty,
+          !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+      errorMessage = "Please Fill In All Fields"
+      return false
+    }
+    guard email.contains("@") && email.contains(".") else {
+      errorMessage = "Please enter valid email"
+      return false
+    }
+    return true
+  }
+}
