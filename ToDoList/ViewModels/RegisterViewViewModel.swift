804cb4b91109b93da1e702dee302c44c678e1a74
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-05-22 01:17:16 +0300

    register new user handler added

--- ToDoList/ViewModels/RegisterViewViewModel.swift
+++ ToDoList/ViewModels/RegisterViewViewModel.swift
@@ -5,4 +5,58 @@
 //  Created by Yusuf Berkay Girgin on 21.05.2023.
 //
 
+import FirebaseFirestore
+import FirebaseAuth
 import Foundation
+
+class RegisterViewViewModel: ObservableObject {
+  @Published var name = ""
+  @Published var email = ""
+  @Published var password = ""
+  
+  init() {}
+  
+  func register() {
+    guard validate() else {
+      return
+    }
+    
+    // create a user
+    Auth.auth().createUser(withEmail: email, password: password) { [weak self] result, error in
+      guard let userId = result?.user.uid else {
+        return
+      }
+      self?.insertUserRecord(id: userId)
+    }
+  }
+  
+  private func insertUserRecord(id: String) {
+    let newUser = User(id: id,
+                       name: name,
+                       email: email,
+                       joined: Date().timeIntervalSince1970)
+    
+    let db = Firestore.firestore()
+    
+    db.collection("users")
+      .document(id)
+      .setData(newUser.asDictionary())
+    
+  }
+  
+  private func validate() -> Bool {
+    guard !name.trimmingCharacters(in: .whitespaces).isEmpty &&
+            !email.trimmingCharacters(in: .whitespaces).isEmpty &&
+            !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+      return false
+    }
+    guard email.contains("@") && email.contains(".") else {
+      return false
+    }
+    
+    guard password.count >= 6 else {
+      return false
+    }
+    return true
+  }
+}
