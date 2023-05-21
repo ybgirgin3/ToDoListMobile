3e082a32513fd3e03bfcd5d7755d72853ec4505e
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-05-22 01:17:08 +0300

    firestore auth handler added

--- ToDoList/ViewModels/MainViewViewModel.swift
+++ ToDoList/ViewModels/MainViewViewModel.swift
@@ -5,4 +5,26 @@
 //  Created by Yusuf Berkay Girgin on 21.05.2023.
 //
 
+import FirebaseAuth
 import Foundation
+
+
+class MainViewViewModel: ObservableObject {
+  @Published var currentUserId: String = ""
+  private var handler: AuthStateDidChangeListenerHandle?
+  
+  init() {
+    let handler = Auth.auth().addIDTokenDidChangeListener { [weak self] _, user in
+      DispatchQueue.main.async {
+        self?.currentUserId = user?.uid ?? ""
+      }
+    }
+  }
+  
+  
+  public var isSignedIn: Bool {
+    return Auth.auth().currentUser != nil
+  }
+  
+  
+}
