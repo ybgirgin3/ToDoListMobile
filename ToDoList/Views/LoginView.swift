d6b3a860cb605a0c878c06c45dc1d868f81eab97
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-05-22 00:27:34 +0300

    login view now uses viewModel

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -8,33 +8,46 @@
 import SwiftUI
 
 struct LoginView: View {
-  @State var email = ""
-  @State var password = ""
+  @StateObject var viewModel = LoginViewViewModel()
+  // @State var email = ""
+  // @State var password = ""
   
   var body: some View {
     NavigationView {
       VStack {
         // Header
-        HeaderView()
+        HeaderView(title: "To Do List",
+                   subTitle: "Get Things Done" ,
+                   angle: 15,
+                   background: Color.pink)
         // Login Form
+        // * error message *
+
+        
         Form {
-          TextField("Email Address", text: $email)
-            .textFieldStyle(RoundedBorderTextFieldStyle())
           
-          SecureField("Password", text: $password)
-            .textFieldStyle(RoundedBorderTextFieldStyle())
+          if !viewModel.errorMessage.isEmpty {
+            Text(viewModel.errorMessage)
+              .foregroundColor(Color.red)
+          }
           
-          Button {
-            // Attemp Login
-          } label: {
-            ZStack {
-              RoundedRectangle(cornerRadius: 10)
-              
-              Text("Log In")
-                .foregroundColor(Color.white)
-                .bold()
-            }
-          }.padding()
+          TextField("Email Address", text: $viewModel.email)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .autocapitalization(.none)
+            .autocorrectionDisabled()
+          
+          SecureField("Password", text: $viewModel.password)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .autocapitalization(.none)
+            .autocorrectionDisabled()
+          
+          TLButton(
+            title: "Log In",
+            background: .blue
+          ) {
+            viewModel.login()
+          }
+          .padding()
           
         }
         
