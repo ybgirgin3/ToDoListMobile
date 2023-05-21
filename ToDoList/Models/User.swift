99a4ecc42a6e7bf8ed88a9386cdb553084327013
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-05-22 01:16:34 +0300

    user model added for db user validation

--- ToDoList/Models/User.swift
+++ ToDoList/Models/User.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+
+struct User: Codable {
+  let id: String
+  let name: String
+  let email: String
+  let joined: TimeInterval
+}
---
+++ ToDoList/Other/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  ToDoList
+//
+//  Created by Yusuf Berkay Girgin on 22.05.2023.
+//
+
+import Foundation
